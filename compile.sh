c802568f13feecadcb08eaf673588475cc6355f2
Aakash Tamboli <athanhuntak@gmail.com>
2023-03-18 19:39:53 +0530

    setting up a envirnoment

---
+++ compile.sh
@@ -0,0 +1,1 @@
+gcc $1 -o output.out -I ../virtual_file_system -I ../Data-Structure-Algorithm-Library/include/ -L ../Data-Structure-Algorithm-Library/lib -llinuxDsAlgo
--- vfs_header.h
+++ vfs_header.h
@@ -2,5 +2,8 @@
 #define VFS_HEADER 1
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #include<stdio_ext.h>
+#include<fcntl.h>
+#include<unistd.h>
 #endif
